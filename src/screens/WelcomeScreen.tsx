511a29219ae5304a1da12d1a233ff3931bb86268
shitikyan <shitikyan@gmail.com>
2023-12-22 16:30:16 +0400

    fixed typo

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView, Text } from "react-native";
 export default function WelcomeScreen() {
   return (
     <SafeAreaView>
-      <Text className="bg-white">Welcome WelcomeWelcome screen</Text>
+      <Text className="bg-white">Welcome screen</Text>
     </SafeAreaView>
   );
 }
